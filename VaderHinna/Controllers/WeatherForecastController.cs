1316daac6c93235d9d03345b7cc8bec4c4eee39a
gregorysl <gregorysl@gmail.com>
2020-01-08 22:37:26 +0100

    Add base project

---
+++ VaderHinna/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VaderHinna.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}

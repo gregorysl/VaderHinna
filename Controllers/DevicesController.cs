470a57187ac8be11305851bc68e4f1bd1f12fb71
gregorysl <gregorysl@gmail.com>
2020-01-19 22:23:54 +0100

    Update packages and add skeleton of controller

---
+++ Controllers/DevicesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VaderHinna.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/devices")]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ILogger<DevicesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{deviceId}/data/{date}/{sensor}")]
+        public string Get(string deviceId, string date, string sensor)
+        {
+            return $"Hello World!{deviceId} {date} {sensor}";
+        }
+    }
+}

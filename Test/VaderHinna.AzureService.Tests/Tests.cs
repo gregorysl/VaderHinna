f4d942faf0c56287bb0fb2b5ebe89a1484f717a5
gregorysl <gregorysl@gmail.com>
2020-01-24 17:47:58 +0100

    Added test for Discovery, fixed typo

--- Test/VaderHinna.AzureService.Tests/Tests.cs
+++ Test/VaderHinna.AzureService.Tests/Tests.cs
@@ -22,31 +22,38 @@ namespace VaderHinna.AzureService.Tests
             _azureConnector = new AzureConnector(_azureSetup.ConnectionString, _azureSetup.ContainerName, null,
                 _csvService);
         }
-        
+
         [Test]
-        public async Task BlobForUrlExist_DoesntExisit()
+        public async Task BlobForUrlExist_DoesntExist()
         {
             var uri = new Uri($"{_azureConnector.BaseUrl}randomBlob");
             var check = await _azureConnector.BlobForUrlExist(uri);
             Assert.AreEqual(false, check);
         }
-        
+
         [Test]
-        public async Task BlobForUrlExist_Exisit()
+        public async Task BlobForUrlExist_Exist()
         {
             var uri = new Uri($"{_azureConnector.BaseUrl}dockan/humidity/2019-01-10.csv");
             var check = await _azureConnector.BlobForUrlExist(uri);
             Assert.AreEqual(true, check);
         }
-        
-        [Test,Ignore("for now Azure Storage Emulator doesn't support Append Blobs. Unignore when testing using other connectionstring")]
-        public async Task DownloadTextByAppendUri_Exisit()
+
+        [Test, Ignore("for now Azure Storage Emulator doesn't support Append Blobs. Unignore when testing using other connectionstring")]
+        public async Task DownloadTextByAppendUri_Exist()
         {
             var uri = new Uri($"{_azureConnector.BaseUrl}dockan/SmallSample.txt");
             var check = await _azureConnector.DownloadTextByAppendUri(uri);
             Assert.AreEqual(true, check);
         }
-        
+
+        [Test]
+        public async Task DeviceDiscovery_DoesntExist()
+        {
+            var devices = await _azureConnector.DeviceDiscovery();
+            Assert.AreEqual(null, devices);
+        }
+
         [TearDown]
         public void TearDown()
         {
